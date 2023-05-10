85ace7e1a0f0d71787b16564ddb2474cb77c1fa3
amirb222 <121934738+amirb222@users.noreply.github.com>
2023-05-10 16:05:19 +0300

    main.py

--- main.py
+++ main.py
@@ -1,55 +1,56 @@
-from flask import Flask, request, jsonify
 from datetime import datetime
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 parking_lot = {}
+ticketId_counter = 0
 
 
 def calculate_charge(entry_time):
+    """
+    Calculates the parking charge based on the entry time.
+    """
     exit_time = datetime.now()
-    parked_time = exit_time - entry_time
-    parked_hours = parked_time.total_seconds() / 3600
-    charged_hours = int(parked_hours) + 1 if parked_hours % 1 > 0 else int(parked_hours)
-    charge = charged_hours * 10
-    return charge
+    total_time = (exit_time - entry_time).total_seconds() / 60  # calculate total time in minutes and
+    # round down to the nearest integer
+    periods = (total_time // 15) + 1  # calculate the number of 15-minute periods
+    charge = periods * 2.5  # update charge to 2.5 dollars per 15 minutes
+    return round(charge, 2)
 
 
-@app.route('/entry', methods=['POST'])
+@app.route('/entry', methods=['GET', 'POST'])
 def entry():
+    """
+    Record the entry time, license plate, and parking lot number.
+    Returns a ticket ID.
+    """
+    global ticketId_counter
     plate = request.args.get('plate')
     parking_lot_id = request.args.get('parkingLot')
     entry_time = datetime.now()
+    ticket_id = str(ticketId_counter)
+    parking_lot[ticket_id] = {'plate': plate, 'parking_lot_id': parking_lot_id, 'entry_time': entry_time}
+    ticketId_counter += 1
+    return jsonify({'ticket_id': ticket_id})
 
-    ticket_id = len(parking_lot) + 1
-    parking_lot[ticket_id] = {'plate': plate, 'entry_time': entry_time, 'parking_lot_id': parking_lot_id}
 
-    return jsonify({'ticketId': ticket_id}), 200
-
-
-@app.route('/exit', methods=['POST'])
+@app.route('/exit', methods=['GET', 'POST'])
 def exit():
-    ticket_id = int(request.args.get('ticketId'))
-
-    if ticket_id in parking_lot:
-        ticket = parking_lot[ticket_id]
-        entry_time = ticket['entry_time']
-        charge = calculate_charge(entry_time)
-
-        response = {
-            'licensePlate': ticket['plate'],
-            'parkedTime': str(datetime.now() - entry_time),
-            'parkingLotId': ticket['parking_lot_id'],
-            'charge': charge
-        }
-
-        del parking_lot[ticket_id]
-
-        return jsonify(response), 200
-    else:
-        return jsonify({'error': 'Invalid ticketId'}), 404
+    """
+    Record the exit time and calculate the parking charge.
+    Returns the license plate, total parked time, parking lot ID, and charge.
+    """
+    ticket_id = request.args.get('ticketId')
+    enter = parking_lot.get(ticket_id)
+    if not enter:
+        return jsonify({'error': 'Ticket not found'}), 404
+    exit_time = datetime.now()
+    charge = calculate_charge(enter['entry_time'])
+    del parking_lot[ticket_id]
+    return jsonify({'plate': enter['plate'], 'total_time': str(exit_time - enter['entry_time']),
+                    'parking_lot_id': enter['parking_lot_id'], 'charge': charge})
 
 
 if __name__ == '__main__':
     app.run()
-
